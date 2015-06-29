02fe8147d6642b2847491f3a33d9377a96a66cc1
Sid Price <sidprice@softtools.com>
2015-06-29 15:42:05 -0600

    Start of project, contains no valid code

---
+++ MorningClock.ino
@@ -0,0 +1,15 @@
+//
+// LED Morning Clock
+//
+// Copyright Sid Price 2015 - All rights reserved
+//
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
